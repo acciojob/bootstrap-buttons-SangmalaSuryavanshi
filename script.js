94d584d596400104b84ccfceb3004af56e0013a7
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-10-05 22:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-//your code here
-cy.get("#button-6").then($button => {
-    console.log($button); // Inspect the button in the console
+describe('Button Tests', () => {
+    it('should check the disabled info button', () => {
+        cy.visit(baseUrl + "/main.html");
+
+        // Check that button has the expected classes and is disabled
+        cy.get("#button-6")
+            .should("have.class", "btn")        // Check for 'btn' class
+            .and("have.class", "btn-info")      // Check for 'btn-info' class
+            .and("be.disabled");                 // Check for disabled state
+    });
+});
+describe('Button Tests', () => {
+    it('should check the disabled info button', () => {
+        cy.visit(baseUrl + "/main.html");
+
+        // Check that button has the expected classes and is disabled
+        cy.get("#button-6")
+            .should("have.class", "btn")        // Check for 'btn' class
+            .and("have.class", "btn-info")      // Check for 'btn-info' class
+            .and("be.disabled");                 // Check for disabled state
+    });
 });
