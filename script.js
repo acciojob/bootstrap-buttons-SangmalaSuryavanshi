63f4a99193219e7655bd0b5e5eaa48a13c8bbdc7
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-10-05 22:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+() => {
+    cy.visit(baseUrl + "/main.html");
+    cy.get("#button-6")
+        .should("have.class", "btn")
+        .and("have.class", "btn-info")
+        .and("be.disabled"); // Check if the button is disabled
+}
+
